c99f62363e81723be6666bb7c5cd5c0f8cc00e33
Sandesh Arsud <100332504+arsudsandesh97@users.noreply.github.com>
2025-09-27 11:50:20 +0530

    Update Solution.sql

--- Basic Joins/Average Time of Process per Machine/Solution.sql
+++ Basic Joins/Average Time of Process per Machine/Solution.sql
@@ -1,10 +1,10 @@
 SELECT
 t1.machine_id,
-ROUND(AVG(t2.timestamp t1.timestamp), 3) as processing_time
+ROUND(AVG(t2.timestamp - t1.timestamp), 3) as processing_time
 FROM
 Activity t1
 JOIN Activity t2 ON t1.machine_id = t2.machine_id
-AND tl.activity_type = 'start'
+AND t1.activity_type = 'start'
 AND t2.activity_type = 'end'
 GROUP BY
 t1.machine_id

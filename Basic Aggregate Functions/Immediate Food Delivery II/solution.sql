d3214b9fdc2333123f3e16489520557fd7107915
Sandesh Arsud <100332504+arsudsandesh97@users.noreply.github.com>
2025-09-27 14:32:59 +0530

    Create solution.sql

---
+++ Basic Aggregate Functions/Immediate Food Delivery II/solution.sql
@@ -0,0 +1,9 @@
+WITH order_rank AS (
+  SELECT *,
+         ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS orank
+  FROM Delivery
+)
+SELECT 
+  ROUND(100 * SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END) / COUNT(*), 2) AS immediate_percentage
+FROM order_rank
+WHERE orank = 1;

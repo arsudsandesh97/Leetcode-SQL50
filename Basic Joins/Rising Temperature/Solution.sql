ab24329809e1a1f30bd3d6c337de43ebfd2561b1
Sandesh Arsud <100332504+arsudsandesh97@users.noreply.github.com>
2025-09-27 11:54:14 +0530

    Update Solution.sql

--- Basic Joins/Rising Temperature/Solution.sql
+++ Basic Joins/Rising Temperature/Solution.sql
@@ -1,5 +1,5 @@
 SELECT t1.id as id
 FROM Weather t1
 JOIN Weather t2
-WHERE DATEDIFF(t1.recordDate t2.recordDate) = 1
+WHERE DATEDIFF(t1.recordDate,t2.recordDate) = 1
 AND t1.temperature > t2.temperature

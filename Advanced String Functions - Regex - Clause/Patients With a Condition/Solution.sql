9fb3edbff491f238cfc073881987a4d9b67ba777
Sandesh Arsud <100332504+arsudsandesh97@users.noreply.github.com>
2025-09-29 12:08:27 +0530

    Update Solution.sql

--- Advanced String Functions - Regex - Clause/Patients With a Condition/Solution.sql
+++ Advanced String Functions - Regex - Clause/Patients With a Condition/Solution.sql
@@ -1,6 +1,9 @@
-SELECT
-patient_id,
-patient_name,
+SELECT 
+patient_id, 
+patient_name, 
 conditions
-FROM Patients
-WHERE conditions LIKE '%DIAB100%'
+FROM 
+Patients
+WHERE 
+conditions LIKE 'DIAB1%'     
+OR conditions LIKE '% DIAB1%' 

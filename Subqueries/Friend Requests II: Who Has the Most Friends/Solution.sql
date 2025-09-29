fdda2c9b2e9fa29c8e9a7d2a9e560f64ec2e22af
Sandesh Arsud <100332504+arsudsandesh97@users.noreply.github.com>
2025-09-29 14:23:52 +0530

    Update Solution.sql

--- Subqueries/Friend Requests II: Who Has the Most Friends/Solution.sql
+++ Subqueries/Friend Requests II: Who Has the Most Friends/Solution.sql
@@ -1,1 +1,9 @@
-
+SELECT id, COUNT(friend) AS num
+FROM (
+  SELECT requester_id AS id, accepter_id AS friend FROM RequestAccepted
+  UNION ALL
+  SELECT accepter_id AS id, requester_id AS friend FROM RequestAccepted
+) RA
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
